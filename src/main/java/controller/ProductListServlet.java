a3bbb4c6d62a345d549bccfc09cfd550073cf8ea
Giuseppe Rossetti <g.rossetti6@studenti.unisa.it>
2025-06-21 11:14:37 +0200

    Completato pagina prodotto, e aggiunto funzionalità breadcrumb

--- src/main/java/controller/ProductListServlet.java
+++ src/main/java/controller/ProductListServlet.java
@@ -4,19 +4,32 @@ package controller;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import model.Bean.Category;
 import model.Bean.Product;
+import model.DAO.CategoryDAO;
 import model.DAO.ProductDAO;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @WebServlet(name = "ProductListServlet", value = "/ProductListServlet")
 public class ProductListServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String brandIdParam = request.getParameter("brandId");
         String categoryIdParam = request.getParameter("categoryId");
         String offersParam = request.getParameter("offers");
+        boolean filterQuery = Boolean.parseBoolean(request.getParameter("filter"));
+
+        ServletContext context = getServletContext();
+        CategoryServlet.checkCategoryCache(context);
+        Map<Integer, Category> categoryCacheMap = (Map<Integer, Category>) context.getAttribute("categoryCacheMap");
         ProductDAO productDAO = new ProductDAO();
         RequestDispatcher dispatcher;
 
