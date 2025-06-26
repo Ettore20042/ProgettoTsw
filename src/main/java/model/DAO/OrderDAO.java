793ee68afcd5c5f78fb63ca4a42e6813ed4da349
Giuseppe Rossetti <g.rossetti6@studenti.unisa.it>
2025-06-26 21:18:10 +0200

    Completato metodo per prendere gli utlimi ordini di un user

--- src/main/java/model/DAO/OrderDAO.java
+++ src/main/java/model/DAO/OrderDAO.java
@@ -2,6 +2,7 @@ package model.DAO;
 
 import com.oracle.wls.shaded.org.apache.xpath.operations.Or;
 import model.Bean.Order;
+import model.Bean.OrderItem;
 import model.Bean.Product;
 import model.ConnPool;
 
@@ -12,6 +13,7 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
 
 public class OrderDAO {
@@ -40,9 +42,42 @@ public class OrderDAO {
 
     public List<Order> doRetrieveLastOrders(int userId) {
         try(Connection connection = ConnPool.getConnection()){
-            PreparedStatement preparedStatement = connection.prepareStatement()
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT o.OrderId, o.Status, o.OrderDate, oi.ProductID " +
+                    "FROM orders o JOIN order_item oi ON o.OrderID = oi.OrderID " +
+                    "WHERE o.OrderID IN (SELECT OrderID FROM orders WHERE UserID = ? ORDER BY OrderDate DESC LIMIT 4) " +
+                    "ORDER BY o.OrderDate DESC, o.OrderID");
+            preparedStatement.setInt(1, userId);
+            ResultSet rs = preparedStatement.executeQuery();
 
+            List<Order> orders = new ArrayList<>();
+            Order currentOrder = null;
+            List<OrderItem> currentItems = null;
+            int lastOrderId = -1;
 
+            while (rs.next()) {
+                int orderId = rs.getInt("OrderID");
+
+                if (orderId != lastOrderId) {
+                    currentOrder = new Order();
+                    currentOrder.setOrderId(orderId);
+                    currentOrder.setStatus(rs.getString("Status"));
+                    currentOrder.setOrderDate(LocalDate.parse(rs.getString("OrderDate")));
+
+                    currentItems = new ArrayList<>();
+                    currentOrder.setOrderItems(currentItems);
+
+                    orders.add(currentOrder);
+                    lastOrderId = orderId;
+                }
+
+                OrderItem item = new OrderItem();
+                item.setProductId(rs.getInt("ProductID"));
+                item.setOrderId(orderId);
+                if (currentItems != null) {
+                    currentItems.add(item);
+                }
+            }
+            return orders;
         }catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
